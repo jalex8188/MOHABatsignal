9b59023b39e152e4a1cae3b4491d0714f3742be3
BuildTools <unconfigured@null.spigotmc.org>
2019-01-10 19:25:09 -0700

    Updated Home.py, removed old code

--- MOHABatsignalHome.py
+++ MOHABatsignalHome.py
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 import json
 from threading import Thread
 from gpiozero import LED, Button # Import GPIO library: https://gpiozero.readthedocs.io/en/stable/
@@ -9,7 +7,7 @@ from time import sleep
 from losantmqtt import Device # Import Losant library: https://github.com/Losant/losant-mqtt-python
 
 led_gpio = 23
-spot_gpio = 24
+#spot_gpio = 24
 button_gpio = 21
 
 led = LED(led_gpio)
@@ -25,36 +23,31 @@ def on_command(device, command):
 
     # Listen for the gpioControl. This name configured in Losant
 	global active
-	if command["name"] == "toggle":
-        # toggle the LED
-	   #     led.toggle()
-		# spot.toggle()
-		if active == True:
-			active = False
-			led = 0
-			spot = 0
-			print(active)
-		else:
-			active = True
-			led = 1
-			spot = 1
-			print(active)
+	global led
+#	global spot
 
 	if command["name"] == "deactivateLight":
 		active = False
-		led = 0
-		spot = 0
+		led.off()
+#		spot.off()
 		print(active)
-
 	if command["name"] == "activateLight":
 		active = True
-		led = 1
-		spot = 1
+		led.on()
+#		spot.on()
 		print(active)
+
+	if command["name"] == "sendLight":
+		sendLight()
 
 def sendDeviceState():
     print("Sending Device State")
     device.send_state({"button": True})
+
+def sendLight():
+    sleep(1)
+    print("Sending Light State")
+    device.send_state({"light":active})
 
 # Listen for commands.
 device.add_event_observer("command", on_command)
